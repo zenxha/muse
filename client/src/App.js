e95a31fd2ffd9c1bbb605aa863682b973daa0fae
zenxha <komays5042@gmail.com>
2023-02-04 18:05:37 -0800

    reimplement nav mui

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 import Button from './components/Button';
 import SignInForm from './components/SignInForm';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
 
 import { useState } from 'react';
 
