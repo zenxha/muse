953389f0e95cd0e5fb63bef587799ef6dac3b0d5
williamEKim <114537897+williamEKim@users.noreply.github.com>
2023-02-12 08:27:32 +0000

    little names changed

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,7 +6,6 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import styles from './Card.module.css';
-import { minHeight } from '@mui/system';
 
 const bull = (
   <Box
--- client/src/routes/About.js
+++ client/src/routes/About.js
@@ -1,16 +1,12 @@
 import Grid from '@mui/material/Unstable_Grid2';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 
-import Button from '../components/Button';
 import OutlinedCard from '../components/Card';
 import creatorData from '../data/data.json';
-import {useState} from 'react';
+
 import styles from './About.module.css'
 
 
 import '../App.css';
-import { TableRow } from '@mui/material';
 
 const About = () => {
 
@@ -22,28 +18,26 @@ const About = () => {
   console.log(creatorData.data);
   
   return (
-  <Grid container spacing={3}   alignItems="center" justify="center" >
-    <Grid item xs={12}>
-    <a
-        className="App-link"
-        href="https://github.com/zenxha/muse"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Github Repository
+    <Grid container spacing={3}   alignItems="center" justify="center" >
+      <Grid item xs={12}>
+      <a
+          className="App-link"
+          href="https://github.com/zenxha/muse"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Repository
 
-      </a>
-    </Grid>
-    
-    {creatorData.data.map(x => {
-      return <Grid item xs={3} >
-        <OutlinedCard key={x.id} person={x}/>
-         </Grid>
-    })}
+        </a>
+      </Grid>
       
-    
-
-</Grid>
+      {creatorData.data.map(person => {
+        return <Grid item xs={3} >
+          <OutlinedCard key={person.id} person={person}/>
+          </Grid>
+      })}
+        
+  </Grid>
   );
 }
 
