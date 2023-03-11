f7e48029c408e65dfacd3945e187164e8e0afdca
zenxha <komays5042@gmail.com>
2023-03-11 04:28:13 -0800

    user api changes

--- server/mongoose/models/user.js
+++ server/mongoose/models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  age: { type: Number, default: 18 },
+  dob: { type: Date, default: 1065139920000 },
   createdAt: { type: Date, default: Date.now }
 });
 
